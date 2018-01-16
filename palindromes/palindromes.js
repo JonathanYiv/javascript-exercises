3c012a82217d1e03b7663f4df0cd4de78abffbba
Jonathan Yiv <JonathanYiv@gmail.com>
2018-01-16 15:41:58 -0500

    Finished palindromes exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,8 @@
-var palindromes = function() {
-
+var palindromes = function(text) {
+  const regex = new RegExp(/[!,\.\s]/, 'g');
+  const strippedText = text.toLowerCase().replace(regex, "");
+  const reversedText = strippedText.split("").reverse().join("");
+  return reversedText === strippedText ? true : false;
 }
 
 module.exports = palindromes
