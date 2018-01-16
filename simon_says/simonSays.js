0a449a9569742cd3560b28b41e2c356a0840ba73
Jonathan Yiv <JonathanYiv@gmail.com>
2018-01-16 15:32:24 -0500

    Finish Simon Says

--- simon_says/simonSays.js
+++ simon_says/simonSays.js
@@ -1,21 +1,26 @@
-function echo() {
-	
+function echo(text) {
+	return text;
 }
 
-function shout() {
-	
+function shout(text) {
+	return text.toUpperCase();
 }
 
-function repeat() {
-
+function repeat(text, number = 2) {
+	let repetition = text;
+	while(number > 1) {
+		repetition = repetition + ' ' + text;
+		number--;
+	}
+	return repetition;
 }
 
-function pieceOfWord() {
-	
+function pieceOfWord(text, number) {
+	return text.slice(0, number);
 }
 
-function firstWord() {
-	
+function firstWord(text) {
+	return text.split(" ")[0];
 }
 
 function capitalize(word) {
@@ -23,8 +28,14 @@ function capitalize(word) {
 	// This function just capitalizes the word given to it, use in conjunction with titleCreator
 }
 
-function titleCreator() {
-	
+function titleCreator(title) {
+	return capitalize(title.split(" ").map(word => {
+		if(!["and", "over", "the"].includes(word)) {
+			return capitalize(word);
+		} else {
+			return word;
+		}
+	}).join(" "));
 }
 
 module.exports = {
