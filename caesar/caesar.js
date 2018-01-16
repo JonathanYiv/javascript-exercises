c8bcb1b0fdf9b0a0eaebe6cc4bae66b210158b08
Jonathan Yiv <JonathanYiv@gmail.com>
2018-01-16 16:50:18 -0500

    Finish caesar

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,33 @@
 var caesar = function(str, shift) {
-
+  return str.split("").map(letter => {
+    if (letter.match(/[a-zA-Z]/)) {
+      let charCode = letter.charCodeAt(0);
+      if (shift > 0) {
+        for(let i = 0; i < shift; i++) {
+          if (charCode === 122) {
+            charCode = 97;
+          } else if (charCode === 90) {
+            charCode = 65;
+          } else {
+            charCode++;
+          }
+        }
+      } else {
+        for(let i = 0; i > shift; i--) {
+          if (charCode === 97) {
+            charCode = 122;
+          } else if (charCode === 65) {
+            charCode = 90;
+          } else {
+            charCode--;
+          }
+        }
+      }
+      return String.fromCharCode(charCode);
+    } else {
+      return letter;
+    }
+  }).join("");
 }
 
 module.exports = caesar
