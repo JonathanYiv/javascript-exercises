425f336e0b3ca7b225007bef863bd1f15faa97e2
Jonathan Yiv <JonathanYiv@gmail.com>
2018-01-16 16:58:10 -0500

    finish fibonacci

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-var fibonacci = function() {
-
+var fibonacci = function(index) {
+  if(index < 0) {
+    return "OOPS";
+  } else if (index < 3) {
+    return 1;
+  } else {
+    return fibonacci(index - 2) + fibonacci(index - 1);
+  }
 }
 
 module.exports = fibonacci
+
+// 1 1 2 3 5 8 ...
