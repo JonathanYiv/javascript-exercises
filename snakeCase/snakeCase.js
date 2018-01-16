187f10dabd99e3c69eacebfee296aa747bbec778
Jonathan Yiv <JonathanYiv@gmail.com>
2018-01-16 16:02:21 -0500

    Finish snakeCase

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,15 @@
-var snakeCase = function() {
-
+var snakeCase = function(text) {
+  const punctuation = new RegExp(/[?,\.]/, 'g');
+  const camelCase = new RegExp(/([a-z]{2})([A-Z])/, 'g');
+  return text.replace(/-/, " ")
+             .replace(/\.\./g, " ")
+             .trim()
+             .replace(camelCase, "$1 $2")
+             .toLowerCase()
+             .replace(punctuation, "")
+             .split(" ")
+             .join("_");
 }
 
 module.exports = snakeCase
+
